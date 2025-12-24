01ec73270aefe9763433782358827a5338ace912
Shachar Alloni <shachar.alloni@post.runi.ac.il>
2025-12-24 11:41:26 +0200

    check IsSorted and Sierpinski

--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,29 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+
+		double x1 = 0;
+		double y1 = 0;
+
+		double x2 = 1;
+		double y2 = 0;
+
+		double x3 = 0.5;
+		double y3 = 0.866;
+
+		sierpinski(n, x1, x2, x3, y1, y2, y3);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+		if (n == 0) return;
+		StdDraw.line(x1, y1, x2, y2);
+		StdDraw.line(x1, y1, x3, y3);
+		StdDraw.line(x2, y2, x3, y3);
+
+		sierpinski(n - 1, x1, (x1 + x2)/2, (x3 + x1)/2, y1, y2, (y3 + y1)/2); //left
+		sierpinski(n - 1, (x1 + x3)/2, (x2 + x3)/2, x3, (y3 + y1)/2, (y3 + y2)/2, y3); //top
+		sierpinski(n - 1, (x1 + x2)/2, x2, (x3 + x2)/2, y1, y2, (y3 + y2)/2); //right
 	}
 }
