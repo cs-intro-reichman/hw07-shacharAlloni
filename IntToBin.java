567848e00cbd08c5979be86264eac4c172f1cb08
Shachar Alloni <shachar.alloni@post.runi.ac.il>
2025-12-24 10:27:59 +0200

    check Palindrome + IntToBin

--- IntToBin.java
+++ IntToBin.java
@@ -8,7 +8,23 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+        if (x == 0) {
+            return "0";
+        }
+        return recToBinary(x);
+    }
+
+    public static String recToBinary(int x) {
+        if (x == 0) {
+            return "";
+        }
+        
+        if (x % 2 == 0) {
+            return recToBinary(x/2) + "0";
+        }
+
+        else {
+            return recToBinary(x/2) + "1";
+        }
     }    
  }
--- Palindrome.java
+++ Palindrome.java
@@ -7,7 +7,15 @@ public class Palindrome {
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+		int length = s.length();
+		
+		if (length <= 1) {
+			return true;
+		}
+		
+		if (s.charAt(0) != s.charAt(length - 1)) {
+			return false;
+		}
+		return isPalindrome(s.substring(1, length - 1));
     }
 }
